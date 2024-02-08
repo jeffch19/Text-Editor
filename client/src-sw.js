b003e885d5bc94a17d49102d175834b924684e1b
jeffch19 <17jcummings@gmail.com>
2024-02-08 15:11:17 -0500

    added asset caching in src-sw.js, still need to use offlinefallback

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,23 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+registerRoute(
+  ({ request }) => request.destination === 'style' ||
+                  request.destination === 'script' ||
+                  request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'assets-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  })
+);
+
+
 // TODO: Implement asset caching
 registerRoute();
