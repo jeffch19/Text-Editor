25f4e7ab4d730ab2b0a3b35f858e74ea09766eed
jeffch19 <17jcummings@gmail.com>
2024-02-09 11:54:06 -0500

    configured webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: './src-sw.js', 
+        swDest: 'service-worker.js', 
+        maximumFileSizeToCacheInBytes:  5 *  1024 *  1024, 
+      }),
+      new WebpackPwaManifest({
+        name: 'My App',
+        short_name: 'App',
+        description: 'My awesome Progressive Web App!',
+        crossorigin: 'null', 
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96,  128,  192,  256,  384,  512] 
+          },
+          {
+            src: path.resolve('src/assets/large-icon.png'),
+            size: '1024x1024' 
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
